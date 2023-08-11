88effb7166ed2f8ff4ce1e6151318f48169c7060
BeanyTheCoder <alexanderafoko@gmail.com>
2023-08-11 19:10:21 +0000

    changes for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  base: "/YOUR_REPOSITORY_NAME",
   plugins: [react()],
 })
